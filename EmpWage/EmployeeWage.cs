a222c0d25cc7e841f71390053940f26278f38a1b
priyanshu-rathore <priyanshurathore132@gmail.com>
2022-12-19 12:00:37 +0530

    Added : Employee Attendace UC1

--- EmpWage/EmployeeWage.cs
+++ EmpWage/EmployeeWage.cs
@@ -8,6 +8,21 @@ namespace EmpWage
 {
     internal class EmployeeWage
     {
-        
+        public static void employeeWage()
+        {
+            //Attendace 
+            int PRESENT = 1;
+
+            Random random= new Random();
+            int ATTENDANCE = random.Next(0,2);
+
+            if(ATTENDANCE == PRESENT ) {
+                Console.WriteLine("Employee is Present");
+            }
+            else
+            {
+                Console.WriteLine("Employee is Absent");
+            }
+        }
     }
 }
--- EmpWage/Program.cs
+++ EmpWage/Program.cs
@@ -5,6 +5,10 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Employee Wage computation problem");
-        }
+
+            EmployeeWage.employeeWage();
+
+
+    }
     }
 }
