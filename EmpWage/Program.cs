4e60a48a3580cde981a84891bcf300e2f73cdc57
priyanshu-rathore <priyanshurathore132@gmail.com>
2022-12-19 11:54:01 +0530

    Welcome to Employee Wage Computation

---
+++ EmpWage/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace EmpWage
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Employee Wage computation problem");
+        }
+    }
+}
